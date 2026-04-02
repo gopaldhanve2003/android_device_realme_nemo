57c63769ef0469d24d03813dcea77a9dd05164ff
Mashopy <Mashopy@users.noreply.github.com>
2026-04-02 13:27:35 +0530

    nemo: Build stub mediatek-common framework
    Transition from using prebuilt mediatek-common and mediatek-framework
    jars to building them from source stubs via hardware/mediatek. Removes
    the need to extract them as proprietary blobs and cleans up manual
    permission XML copies.
    
    nemo: Build minimal MediaTek {telecom,telephony}-common JARs
    
    * Also remove MediaTek ims-common JAR as nothing uses it.
    
    Change-Id: I95207cae4f0a3b2b62913377c2062e7cf9ff88ce | AOSP
    Signed-off-by: Naveen <133593113+elohim-etz@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -85,6 +85,7 @@ PRODUCT_PACKAGES += \
 
 # IMS
 $(call inherit-product, hardware/lineage/compat/frameworks/compat.mk)
+$(call inherit-product, hardware/mediatek/frameworks/mediatek-frameworks.mk)
 
 # Init
 PRODUCT_PACKAGES += \
@@ -170,13 +171,8 @@ PRODUCT_ENFORCE_RRO_TARGETS += *
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/permissions/mediatek-common.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/mediatek-common.xml \
-    $(LOCAL_PATH)/configs/permissions/mediatek-framework.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/mediatek-framework.xml \
     $(LOCAL_PATH)/configs/permissions/mediatek-ims-base.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/mediatek-ims-base.xml \
-    $(LOCAL_PATH)/configs/permissions/mediatek-ims-common.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/mediatek-ims-common.xml \
-    $(LOCAL_PATH)/configs/permissions/mediatek-telecom-common.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/mediatek-telecom-common.xml \
     $(LOCAL_PATH)/configs/permissions/mediatek-telephony-base.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/mediatek-telephony-base.xml \
-    $(LOCAL_PATH)/configs/permissions/mediatek-telephony-common.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/mediatek-telephony-common.xml \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-mediatek.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-mediatek.xml \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
