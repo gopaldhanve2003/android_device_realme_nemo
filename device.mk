7dc5a94fe77f91fe4aea8c5879ac5d36772f579c
gopaldhanve2003 <143928521+gopaldhanve2003@users.noreply.github.com>
2026-09-14 08:26:59 +0000

    Partially revert "nemo: Adapt for crDroid"
    This reverts commit 2dddc49b6825148c99b9b4f9af6fa682680f7a94.

--- device.mk
+++ device.mk
@@ -40,9 +40,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
 
-# BCR
-$(call inherit-product-if-exists, vendor/bcr/bcr.mk)
-
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth-service.mediatek
@@ -127,9 +124,6 @@ PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
 $(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/mmi_charging_enable)
-
-# Matlog
-TARGET_DISABLE_MATLOG := true
 
 # Media
 PRODUCT_COPY_FILES += \
